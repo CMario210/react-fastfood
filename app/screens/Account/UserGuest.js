afee8a58bd475fdc6ffe41ff970e98b3784e2ff8
Carlos Rodriguez <camario_1121@hotmail.com>
2021-05-28 23:24:26 -0500

    Progress day two

---
+++ app/screens/Account/UserGuest.js
@@ -0,0 +1,69 @@
+import React from "react"
+import { StyleSheet, View, ScrollView, Text, Image } from "react-native"
+import { Button } from "react-native-elements"
+import { useNavigation } from "@react-navigation/native"
+
+export default function UserGuest() {
+    const navigation = useNavigation()
+
+    return (
+        <ScrollView centerContent={true} style={styles.viewBody}>
+            <Image
+                source={require("../../../assets/img/user-guest.jpg")}
+                resizeMode="contain"
+                style={styles.image}
+            />
+            <Text style={styles.title}>Consulta tu perfíl de React-Fastfood</Text>
+            <Text style={styles.description}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit
+                esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                sint occaecat cupidatat non proident, sunt in culpa qui
+                officia deserunt mollit anim id est laborum.
+            </Text>
+            <View style={styles.viewBtn}>
+                <Button
+                    title="Ver tu perfil"
+                    buttonStyle={styles.btnStyle}
+                    containerStyle={styles.btnContainer}
+                    onPress={() => navigation.navigate("login")}
+                />
+            </View>
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewBody: {
+        marginLeft: 30,
+        marginRight: 30,
+    },
+    image: {
+        height: 300,
+        width: "100%",
+        marginBottom: 40,
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 19,
+        marginBottom: 10,
+        textAlign: "center",
+    },
+    description: {
+        textAlign: "center",
+        marginBottom: 20,
+    },
+    viewBtn: {
+        flex: 1,
+        alignItems: "center",
+    },
+    btnStyle: {
+        backgroundColor: "#00A680",
+    },
+    btnContainer: {
+        width: "70%",
+    },
+})
