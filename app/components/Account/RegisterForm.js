cf89f9e8f13a0c9b78d5aae209a8a750ba1e4d69
Carlos Rodriguez <camario_1121@hotmail.com>
2021-06-01 23:36:20 -0500

    progreso día 5

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -2,14 +2,26 @@ import React, { useState } from "react"
 import { StyleSheet, View } from "react-native"
 import { Input, Icon, Button } from "react-native-elements"
 import { validateEmail } from "../../utils/validations"
+import { size, isEmpty } from "lodash"
 
-export default function RegisterForm() {
+export default function RegisterForm(props) {
+    const { toastRef } = props
     const [showPassword, setShowPassword] = useState(false)
     const [showRepeatPassword, setShowRepeatPassword] = useState(false)
     const [formData, setFormData] = useState(defaultFormValue())
 
     const onSubmit = () => {
-
+        if (isEmpty(formData.email) || isEmpty(formData.password) || isEmpty(formData.repeatPassword)) {
+            toastRef.current.show("Todos los campos son obligatorios")
+        } else if(!validateEmail(formData.email)) {
+            toastRef.current.show("El email no es correcto")
+        } else if(formData.password !== formData.repeatPassword) {
+            toastRef.current.show("Las contraseñas tienen que ser iguales")
+        } else if(size(formData.password) < 6) {
+            toastRef.current.show("La contraseña tiene que tener al menos 6 caracteres")
+        } else {
+            console.log("ok")
+        }
     }
 
     const onChange = (e, type) => {
